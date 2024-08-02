0bef4e2e22b8d5bdb08ca7689b562b3598aa9857
22123521 <22123521@ITWINLAB-8>
2024-08-02 08:32:04 +0000

    Add project files.

---
+++ Models/Booking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ClientBook.Models
+{
+    public class Booking
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int bookingid { get; set; }
+        public int clientid { get; set; }
+        public virtual Client Client { get; set; } //Foreign Key
+
+        public DateTime bookingday { get; set; }
+
+        public string description { get; set; }
+    }
+}
